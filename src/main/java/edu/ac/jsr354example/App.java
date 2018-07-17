93e53dcaad9ff12359eb9aa1e5a51e8d622afac8
Jonathan Ohara <jaraujo@squaretrade.com>
2018-07-17 20:04:02 -0300

    Snippet update

--- src/main/java/edu/ac/jsr354example/App.java
+++ src/main/java/edu/ac/jsr354example/App.java
@@ -1,11 +1,21 @@
 package edu.ac.jsr354example;
 
+import org.javamoney.moneta.Money;
+import org.javamoney.moneta.format.MonetaryAmountDecimalFormatBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.money.format.MonetaryAmountFormat;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+
+
+        MonetaryAmountFormat builderFormat = MonetaryAmountDecimalFormatBuilder.of("R$ ###,###.00").build();
+
+        System.out.println(builderFormat.format(Money.of(1000.22, "BRL")));
+//        SpringApplication.run(App.class, args);
+
     }
 }
